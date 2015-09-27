84558c9775a74ff7bd8d574866e0dd698a1aeaa4
Neis Gwapo <mike.vince47@gmail.com>
2015-09-27 16:17:54 +0800

    changing config

--- include/config.php
+++ include/config.php
@@ -1,13 +1,13 @@
 <?php 
 error_reporting(0);
-define('base_url',''); // please prepend 'http://' and append '/' along with the Domain address
+define('base_url','http://ezifunerals.com.au/'); // please prepend 'http://' and append '/' along with the Domain address
 
 define('folder_name','/');
 
-$hostname = "localhost"; 
-$db_user = "root"; 
+$hostname = "mysql-5.ezifunerals.com.au"; 
+$db_user = "myezif1002"; 
 //$db_password = "R5qQT6bb"; 
-$db_password = "6yhn7ujm8ik,";
+$db_password = "R5qQT6bb,";
 $database = "ezifunerals_ezifunerals_com_au";  
 $db = mysql_connect($hostname, $db_user, $db_password); 
 mysql_select_db($database,$db);	
